89048c158916fcd8e4c3252e57a9b2f7f4489264
Alex Jarvis <alxjrvs@users.noreply.github.com>
2026-07-15 16:28:52 -0400

    chore(formula): boom v0.17.0 (#87)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/boom.rb
+++ Formula/boom.rb
@@ -11,22 +11,22 @@ class Boom < Formula
   on_macos do
     on_arm do
       url "https://github.com/alxjrvs/boom/releases/download/v#{version}/boom-bun-darwin-arm64"
-      sha256 "c477e43e645a5e727b3c9ac23fdb40d21445883cc19bb020f1826b213977ee15"
+      sha256 "d0fcf47cf1cb4ae1b0c24234076b3656057b805a4ad93db3a64409a0a3a232ac"
     end
     on_intel do
       url "https://github.com/alxjrvs/boom/releases/download/v#{version}/boom-bun-darwin-x64"
-      sha256 "d05b241c502cb05fd9e76076126866ff3598d7330d4bd05ea515061d56418061"
+      sha256 "828e71425d0fe8a55ffb87fb35969fdb4ffb2677dc2beecdfdf5fe080bc7f3a0"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/alxjrvs/boom/releases/download/v#{version}/boom-bun-linux-arm64"
-      sha256 "725012d7343b24e61951e0fb1283ec3e550c6d7e39d9f61d54bcf31e5b44068c"
+      sha256 "0722aa20dc35daf3be2759dc46a486d5d4c078030c923f5cae7e282b793eb596"
     end
     on_intel do
       url "https://github.com/alxjrvs/boom/releases/download/v#{version}/boom-bun-linux-x64"
-      sha256 "f0baf3a0a5d517b6c2e3e9e11cf9aa40966b6da101cd5e10aba6a9f67ef7c1ce"
+      sha256 "1029514635ed50871dff01d0aea20c282a7627296a7e4b26bb40cb2828fd3a57"
     end
   end
 
